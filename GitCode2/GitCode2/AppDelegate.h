1f046d8ea8d531f7ef8a3dee1b4da6ead4a6f715
高文杰 <yidong-ios16@yhd.com>
2016-11-19 16:06:37 +0800

    init

---
+++ GitCode2/GitCode2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitCode2
+//
+//  Created by wenjie on 16/11/19.
+//  Copyright © 2016年 OTS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitCode2/GitCode2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitCode2
+//
+//  Created by wenjie on 16/11/19.
+//  Copyright © 2016年 OTS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
